b5b29b2ce4197b38bf72eab00d66e83a7bf26c49
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-09-23 16:35:28 +0200

    start adding router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 import Table from './courses/Table'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 class App extends Component {
   render () {
     return (
-      <Table/>
+      <Router>
+        <Switch>
+          <Route exact path="/"> <Table/> </Route>
+          <Route path="/course/:code">
+            <div>hej</div>
+          </Route>
+        </Switch>
+      </Router>
     )
   }
 }
---
+++ front-end/src/courses/DataManagement.js
@@ -0,0 +1,20 @@
+import pureData from './pureScrapeData'
+
+const scrapeData = Object.values(pureData).filter((item, index) => index !== 2)
+const scrapeKeys = Object.keys(pureData[0]).filter((item, index) => index !== 2)
+
+export let orderedHeaders = [scrapeKeys[4], ...scrapeKeys.slice(0, 4), ...scrapeKeys.slice(5)]
+export const latestData = scrapeData.reduce((acc, processing, ind, arr) => {
+  const containsCurrCourse = acc.filter(item => item.code === processing.code).length > 0
+
+  if (containsCurrCourse) {
+    const processingYear = parseInt(processing.year.slice(-2), 10)
+    const notInteresting = acc.filter(current => current.code !== processing.code)
+    const interesting = acc.filter(current => current.code === processing.code)
+    return interesting
+      .map(current => parseInt(current.year.slice(-2), 10) > processingYear ? current : processing)
+      .concat(notInteresting)
+  }
+  return acc.concat(processing)
+}, [])
+
