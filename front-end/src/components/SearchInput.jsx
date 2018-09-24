fe171a1147d22678003136add77261f539a81582
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-09-24 20:32:06 +0200

    Simple search

--- front-end/src/components/SearchInput.jsx
+++ front-end/src/components/SearchInput.jsx
@@ -4,11 +4,13 @@ import styled from 'styled-components'
 const DamnFineInput = styled.input`
  width: 300px; 
  height: 1.5em;
+ grid-area: ${props => props.gridArea};
 `
 
-const SearchField = ({onChange, value}) => <DamnFineInput
+const SearchField = ({onChange, value, gridArea}) => <DamnFineInput
   value={value}
   onChange={onChange}
+  gridArea={gridArea}
   placeholder='Search by name or course code'/>
 
 export default SearchField
--- front-end/src/containers/TablePage.jsx
+++ front-end/src/containers/TablePage.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
 import { latestData, orderedHeaders } from '../data/DataManagement'
 import Table from '../components/Table'
 import SearchInput from '../components/SearchInput'
+
+const TableContainer = styled.div`
+ display: grid;
+ grid-template-areas: ".     header ."
+                      ".     search ."
+                      "table table  table";
+ grid-template-rows: 50px 50px 1fr;
+ grid-template-columns: 100px 1fr 1fr;
+`
+
+const AreaWrapper = styled.div`
+  grid-area: ${props => props.gridArea};
+ `
 
 class TablePage extends React.Component {
   constructor(props) {
@@ -16,12 +30,24 @@ class TablePage extends React.Component {
 
   render() {
     return (
-      <div>
-        <SearchInput
-          onChange={(event) => this.setState({searchTerm: event.target.value})}
-          value={this.state.searchTerm}/>
-        <Table data={this.courseSearch(latestData)} headers={orderedHeaders} placeholder="Search for course code or name"/>
-      </div>
+      <TableContainer>
+        <AreaWrapper gridArea="header">
+          <h1>CEQ-TLDR</h1>
+        </AreaWrapper>
+        <AreaWrapper gridArea="search">
+          <SearchInput
+            onChange={(event) => this.setState({searchTerm: event.target.value})}
+            value={this.state.searchTerm}
+          />
+        </AreaWrapper>
+
+        <AreaWrapper gridArea="table">
+          <Table
+            data={this.courseSearch(latestData)}
+            headers={orderedHeaders}
+          />
+        </AreaWrapper>
+      </TableContainer>
     )
   }
 }
