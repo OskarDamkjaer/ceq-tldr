ddd9f47aeea79d6d4f84d5075639425bda20cdbe
Catarina Sörensen <catarinajayway@Catarinas-MacBook-Pro.local>
2018-09-24 11:29:59 +0200

    added line in course

--- front-end/src/components/Course.jsx
+++ front-end/src/components/Course.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Course = ({ match }) => (
   <div>
-    <h3>{match.params.code}</h3>
+    <h3>{`Sammanställning kurs: ${match.params.code}`}</h3>
     <h3>{match.url}</h3>
   </div>
 );
