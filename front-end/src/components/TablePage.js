38d2be0648e5642cd691868bc2dd072c44402bda
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-09-24 15:59:24 +0200

    Move to container folder

--- front-end/src/components/TablePage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { latestData, orderedHeaders } from '../data/DataManagement'
-import Table from './Table'
-
-class TablePage extends React.Component {
-  render () {return <Table data={latestData} headers={orderedHeaders}/>}
-}
-
-export default TablePage
---
+++ front-end/src/containers/TablePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { latestData, orderedHeaders } from '../data/DataManagement'
+import Table from '../components/Table'
+
+class TablePage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {searchTerm: ''}
+  }
+
+  render() {
+    return (
+      <Table data={latestData} headers={orderedHeaders}/>
+    )
+  }
+}
+
+export default TablePage
