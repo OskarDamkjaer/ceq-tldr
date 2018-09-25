8b0668b70d078100fdd23654598893bc2b456538
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-09-25 21:04:05 +0200

    Fix correct course data

--- front-end/src/components/Course.jsx
+++ front-end/src/components/Course.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import CourseGraph from './CourseGraph'
+import { courseHistory } from '../data/DataManagement'
 
-const Course = ({ match }) => (
+const Course = ({match}) => (
   <div>
     <h3>{`Sammanställning kurs: ${match.params.code.replace(/-/g, ' ')}`}</h3>
-    <CourseGraph />
-)
-
+    <CourseGraph/>
+    <div> {console.log(courseHistory(match.params.code.replace(/-/g, ' ')))} </div>
   </div>
 )
 
--- front-end/src/data/DataManagement.js
+++ front-end/src/data/DataManagement.js
@@ -6,7 +6,6 @@ const scrapeKeys = Object.keys(pureData[0]).filter((item, index) => index !== 2)
 export const orderedHeaders = [scrapeKeys[4], ...scrapeKeys.slice(0, 4), ...scrapeKeys.slice(5)]
 export const latestData = scrapeData.reduce((acc, processing) => {
   const containsCurrCourse = acc.filter(item => item.code === processing.code).length > 0
-
   if (containsCurrCourse) {
     const processingYear = parseInt(processing.year.slice(-2), 10)
     const notInteresting = acc.filter(current => current.code !== processing.code)
@@ -18,3 +17,5 @@ export const latestData = scrapeData.reduce((acc, processing) => {
   }
   return acc.concat(processing)
 }, [])
+
+export const courseHistory = courseCode => scrapeData.filter(item => item.name === courseCode)
