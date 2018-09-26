042a2ee9d5ed1c2479cc3da2540f2f53f14436c2
Catarina Sörensen <catarinajayway@Catarinas-MacBook-Pro.local>
2018-09-26 10:09:37 +0200

    removed all replace func, added real data to graf

--- front-end/src/containers/Course.jsx
+++ front-end/src/containers/Course.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { courseHistory } from '../data/DataManagement'
 import CourseGraph from '../components/CourseGraph'
 
-const Course = ({match}) => (
+const Course = ({ match }) => (
   <div>
-    <h3>{`Sammanställning kurs: ${match.params.code.replace(/-/g, ' ')}`}</h3>
-    <CourseGraph/>
-    <div> {console.log(courseHistory(match.params.code.replace(/-/g, ' ')))} </div>
+    <h3>{`Sammanställning kurs: ${match.params.code}`}</h3>
+    <CourseGraph dataArray={courseHistory(match.params.code)} />
+    <div>
+      {console.log(match.params.code)}
+      {console.log(`the full array${courseHistory(match.params.code)}`)}
+      {' '}
+    </div>
   </div>
 )
 
