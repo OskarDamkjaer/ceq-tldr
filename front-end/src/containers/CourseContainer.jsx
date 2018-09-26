fa7712d3e695ed09433dbdc241bc0d23d1ed3087
Catarina Sörensen <catarinajayway@Catarinas-MacBook-Pro.local>
2018-09-26 16:24:39 +0200

    added graphs for all headers

--- front-end/src/containers/CourseContainer.jsx
+++ front-end/src/containers/CourseContainer.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { courseHistory, nameByCourse } from '../data/DataManagement'
+import { orderedHeaders } from '../data/DataManagement'
 import CourseGraph from '../components/Course/CourseGraph'
 
 
 const CourseContainer = ({ courseCode }) => (
   <div>
     <h3>{`Sammanställning kurs: ${nameByCourse(courseCode)}`}</h3>
-    <CourseGraph dataArray={courseHistory(courseCode)} tag="importanceScore" />
+    {
+      orderedHeaders.slice(1).map(header => (
+        <CourseGraph dataArray={courseHistory(courseCode)} tag={header} />
+      ))
+    }
+    {' '}
     <div />
   </div>
 )
