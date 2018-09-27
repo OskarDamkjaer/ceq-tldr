e27585aeddd7ab965cfd415c594d767d744f07d5
Catarina Sörensen <catarinajayway@Catarinas-MacBook-Pro.local>
2018-09-27 11:44:22 +0200

    real data all header and excluded graphs that is inrelevant

--- front-end/src/containers/CourseContainer.jsx
+++ front-end/src/containers/CourseContainer.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { courseHistory, nameByCourse } from '../data/DataManagement'
-import { orderedHeadersStyled } from '../data/DataManagement'
+import { orderedHeaders, orderedHeadersStyled } from '../data/DataManagement'
 import CourseGraph from '../components/Course/CourseGraph'
 
+const excludedHeaders = ['CODE', 'YEAR', 'POINTS']
+const excludedHeadersData = ['code', 'year', 'points']
+
+const orderedHeadersFiltered = orderedHeaders.filter(header => !excludedHeadersData.includes(header))
+const orderedHeadersStyledFiltered = orderedHeadersStyled.filter(header => !excludedHeaders.includes(header))
 
 const CourseContainer = ({ courseCode }) => (
   <div>
     <h3>{`Sammanställning kurs: ${nameByCourse(courseCode)}`}</h3>
     {
-      orderedHeadersStyled.slice(1).map(header => (
-        <CourseGraph dataArray={courseHistory(courseCode)} tag={header} />
+      orderedHeadersStyledFiltered.slice(1).map((header, index) => (
+        <CourseGraph dataArray={courseHistory(courseCode)} tag={header} dataTag={orderedHeadersFiltered.slice(1)[index]} />
       ))
     }
     {' '}
