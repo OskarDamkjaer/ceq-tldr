360d626659860f7c3ea5af14f8ec2a08638f5cde
Catarina Sörensen <catarinajayway@Catarinas-MacBook-Pro.local>
2018-09-28 14:12:26 +0200

    refactoring

--- front-end/src/components/Course/CourseGraphRegistered.jsx
+++ front-end/src/components/Course/CourseGraphRegistered.jsx
@@ -4,8 +4,6 @@ import { AxisBottom, AxisLeft } from '@vx/axis'
 import { scaleLinear } from '@vx/scale'
 import { curveNatural } from '@vx/curve'
 import { LinePath } from '@vx/shape'
-import GraphHeader from './GraphHeader'
-
 
 const GraphWrapper = styled.div`
  font-size: 1,2em;
@@ -19,40 +17,32 @@ const xScale = scaleLinear({
   domain: [2019, 2012],
 })
 
-const yScale = scaleLinear({
-  rangeRound: [0, 800],
-  domain: [100, -100],
+const yRegScale = scaleLinear({
+  rangeRound: [0, 400],
+  domain: [200, 0],
 })
 
 const xValue = item => parseInt(`20${item.year.substring(3, 5)}`, 10)
+const yValue = (item, dataTag) => parseInt(item[dataTag], 10)
 
-const yValue = (item, dataTag) => parseInt(parseInt(item[dataTag], 10), 10)
-
-const tickArray = [-100, -80, -60, -40, -20, 0, +20, 40, 60, 80, 100]
+const registeredArray = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+  110, 120, 130, 140, 150, 160, 170, 180, 190]
 const tickArrayX = [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
 const colorArray = ['#3F2A36', '#DB2580', '#C5E1A0', '#75BBC0', '#117D69', '#FAE8C4', '#66D594']
-/*
-ASSESMENT, GOAL, IMPORTANCE, PERCENTAGE , SATISFACTIONSCORE, TECH, WORKLOAD
-*/
 
-const CourseGraph = ({
-  graphHeaders, graphHeadersStyled, courseHistoryYears, name,
-}) => (
+const CourseGraphRegistered = ({ courseHistoryYears }) => (
   <GraphWrapper>
-    <GraphHeader graphHeadersStyled={graphHeadersStyled} colorArray={colorArray} name={name} />
     <svg style={{ paddingLeft: '40px', paddingTop: '50px' }} width="600" height="850">
-      {graphHeadersStyled.map((header, index) => (
-        <LinePath
-          data={courseHistoryYears}
-          xScale={xScale}
-          yScale={yScale}
-          x={item => xValue(item)}
-          y={item => yValue(item, graphHeaders[index])}
-          curve={curveNatural}
-          stroke={colorArray[index]}
-          strokeWidth={2}
-        />
-      ))}
+      <LinePath
+        data={courseHistoryYears}
+        xScale={xScale}
+        yScale={yRegScale}
+        x={item => xValue(item)}
+        y={item => yValue(item, 'registered')}
+        curve={curveNatural}
+        stroke="pink"
+        strokeWidth={2}
+      />
       <AxisBottom
         scale={xScale}
         top={400}
@@ -61,11 +51,11 @@ const CourseGraph = ({
         tickValues={tickArrayX}
       />
       <AxisLeft
-        scale={yScale}
-        tickValues={tickArray}
+        scale={yRegScale}
+        tickValues={registeredArray}
       />
     </svg>
   </GraphWrapper>
 )
 
-export default CourseGraph
+export default CourseGraphRegistered
--- front-end/src/containers/CourseContainer.jsx
+++ front-end/src/containers/CourseContainer.jsx
@@ -4,6 +4,7 @@ import {
 } from '../data/DataManagement'
 
 import CourseGraph from '../components/Course/CourseGraph'
+import CourseGraphRegistered from '../components/Course/CourseGraphRegistered'
 
 const CourseContainer = ({ courseCode }) => (
   <div>
@@ -12,6 +13,9 @@ const CourseContainer = ({ courseCode }) => (
       graphHeadersStyled={orderedHeadersStyledFiltered}
       courseHistoryYears={courseHistoryYears(courseCode)}
       name={nameByCourse(courseCode)}
+    />
+    <CourseGraphRegistered
+      courseHistoryYears={courseHistoryYears(courseCode)}
     />
   </div>
 )
