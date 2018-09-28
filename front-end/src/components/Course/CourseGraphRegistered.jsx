b041390dc626cc71eb520948eeab8b15062a8047
Catarina Sörensen <catarinajayway@Catarinas-MacBook-Pro.local>
2018-09-28 16:04:18 +0200

    added number registered graph

--- front-end/src/components/Course/CourseGraphRegistered.jsx
+++ front-end/src/components/Course/CourseGraphRegistered.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { AxisBottom, AxisLeft } from '@vx/axis'
 import { scaleLinear } from '@vx/scale'
-import { curveNatural } from '@vx/curve'
-import { LinePath } from '@vx/shape'
+import { Bar } from '@vx/shape'
 
 const GraphWrapper = styled.div`
  font-size: 1,2em;
@@ -14,7 +13,7 @@ const GraphWrapper = styled.div`
 
 const xScale = scaleLinear({
   rangeRound: [500, 0],
-  domain: [2019, 2012],
+  domain: [2019, 2011],
 })
 
 const yRegScale = scaleLinear({
@@ -22,27 +21,29 @@ const yRegScale = scaleLinear({
   domain: [200, 0],
 })
 
-const xValue = item => parseInt(`20${item.year.substring(3, 5)}`, 10)
-const yValue = (item, dataTag) => parseInt(item[dataTag], 10)
+const graphArray = [53, 116, 179, 242, 305, 368, 431, 494]
+const xStatus = index => graphArray[index]
 
 const registeredArray = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
-  110, 120, 130, 140, 150, 160, 170, 180, 190]
-const tickArrayX = [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
+  110, 120, 130, 140, 150, 160, 170, 180, 190, 200]
+const tickArrayX = ['', 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
+
 const colorArray = ['#3F2A36', '#DB2580', '#C5E1A0', '#75BBC0', '#117D69', '#FAE8C4', '#66D594']
 
 const CourseGraphRegistered = ({ courseHistoryYears }) => (
   <GraphWrapper>
     <svg style={{ paddingLeft: '40px', paddingTop: '50px' }} width="600" height="850">
-      <LinePath
-        data={courseHistoryYears}
-        xScale={xScale}
-        yScale={yRegScale}
-        x={item => xValue(item)}
-        y={item => yValue(item, 'registered')}
-        curve={curveNatural}
-        stroke="pink"
-        strokeWidth={2}
-      />
+      {courseHistoryYears.map((item, index) => (
+        <Bar
+          width={20}
+          height={item.registered}
+
+          x={xStatus(index)}
+          y={400 - item.registered}
+
+          fill={colorArray[index]}
+        />
+      ))}
       <AxisBottom
         scale={xScale}
         top={400}
