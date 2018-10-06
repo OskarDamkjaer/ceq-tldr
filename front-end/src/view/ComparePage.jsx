fdfe35ce662d5735142b43a11b42953f7dd3eb3a
Catarina Sörensen <catarinajayway@Catarinas-MacBook-Pro.local>
2018-10-06 17:55:00 +0200

    compare page

---
+++ front-end/src/view/ComparePage.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import CompareContainer from '../containers/CompareContainer'
+
+const ComparePage = ({ match }) => (
+  <CompareContainer course1={match.params.courses.split(':')[0].toUpperCase()} course2={match.params.courses.split(':')[1].toUpperCase()} />
+)
+
+export default ComparePage
