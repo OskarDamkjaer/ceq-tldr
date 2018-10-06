b371ce4d17cf2cdfe4876aa33e102105066e72d3
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-10-06 13:37:19 +0200

    Add index file

---
+++ front-end/src/data/lastFiveYears/index.js
@@ -0,0 +1,13 @@
+import year13 from './course_data13.js'
+import year14 from './course_data14.js'
+import year15 from './course_data15.js'
+import year16 from './course_data16.js'
+import year17 from './course_data17.js'
+
+export default {
+  year13,
+  year14,
+  year15,
+  year16,
+  year17
+}
