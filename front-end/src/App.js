3a846e9e135ae893d45bcbc861e68f9e4d3a8560
Catarina Sörensen <catarinajayway@Catarinas-MacBook-Pro.local>
2018-10-08 12:42:32 +0200

    compare page now working

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,17 +2,16 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import ColorProvider from './context/ColorProvider'
-import CoursePage from './view/CoursePage'
-//import ComparePage from './view/ComparePage'
-import { historyListForCourseCode, INFOCOM } from './data/dataFetcher'
 import TablePage from './view/TablePage'
+import CoursePage from './view/CoursePage'
+import ComparePage from './view/ComparePage'
 
 const App = () => (
   <ColorProvider>
     <Router>
       <Switch>
-        <Route exact path="/test" render={() => (<div>{historyListForCourseCode('FAFF25', INFOCOM)}</div>)}/>
-        <Route exact path="/" component={TablePage}/>
+        <Route exact path="/" component={TablePage} />
+        <Route exact path="/compare/:courses" component={ComparePage} />
         <Route path="/:code" component={CoursePage} />
       </Switch>
     </Router>
