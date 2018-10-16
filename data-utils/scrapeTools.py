2ef8a37c6a036392009dbe596283b3d7c109a576
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-10-16 12:44:03 +0200

    Under construction

--- data-utils/scrapeTools.py
+++ data-utils/scrapeTools.py
@@ -93,7 +93,7 @@ def get_all_hrefs():
     return urlbois
 
 
-def current_course_codes(start_year):
+def current_course_codes(start_year, save):
     year = str(start_year) + "_" + str(start_year + 1)
     programs = ["C", "D"]
     result = {}
@@ -119,26 +119,46 @@ def current_course_codes(start_year):
             if re.search('[a-zA-Z]{4}[0-9]{2}', text) is not None:
                 if len(text) == 6:
                     result[prog][current_key].append(text)
-    f = open("./data/courses.json", "w")
-    f.write(json.dumps(result))
+            if re.search('[a-zA-Z]{3}[0-9]{3}', text) is not None:
+                if len(text) == 6:
+                    result[prog][current_key].append(text)
+    if save:
+        f = open("./data/courses" + str(start_year) + ".json", "w")
+        f.write(json.dumps(result))
     return result
 
 
-def get_url_by_course_code(code, start_year, program):
+current_course_codes(15, True)
+
+
+def get_url_by_course_code(code, start_year):
     year = str(start_year) + str(int(start_year) + 1)
-    url = "http://www.ceq.lth.se/rapporter/?lasar_lp=" + year + "&program=" + program + "&kurskod=" + code
+    url = "http://www.ceq.lth.se/rapporter/?lasar_lp=" + year + "&program=alla&kurskod=" + code
     raw_html = simple_get(url)
     soup = BeautifulSoup(raw_html, 'html.parser')
     print("Looking at " + str(url) + " for links")
     urlbois = []
+    goodbois = []
     for a in soup.find_all('a'):
         if "slutrapport.html" in a['href']:
             urlbois.append(a['href'])
-    return urlbois  # LETS go only first one
+
+    if len(urlbois) == 0:
+        new_url = "http://www.ceq.lth.se/rapporter/?lasar_lp=" + year + "&program=samtliga&kurskod=" + code
+        new_html = simple_get(new_url)
+        new_soup = BeautifulSoup(new_html, 'html.parser')
+        print("first failed, looking at " + str(url) + " for more links")
+        for a in new_soup.find_all('a'):
+            if "slutrapport.html" in a['href']:
+                urlbois.append(a['href'])
+    for url in urlbois:
+        if code in url:
+            goodbois.append(url)
+    return goodbois  # Finds garbage when course is not found
 
 
 def find_and_save_all_data(start_year):
-    courses = current_course_codes(start_year)
+    courses = current_course_codes(start_year, False)
     save = {}
     for program, data in courses.items():
         save[program] = {}
@@ -146,19 +166,17 @@ def find_and_save_all_data(start_year):
             save[program][year] = []
             for code in codes:
                 print("getting " + code, start_year, program)
-                if "obligatoriska kurser" in year:
-                    url = get_url_by_course_code(code, start_year, program)
-                else:
-                    url = get_url_by_course_code(code, start_year, "")
-
+                url = get_url_by_course_code(code, start_year)
                 print("url is " + str(url))
-                if len(url) == 1:
+                if len(url) > 0: #sometimes there are more than 1 LP
                     save[program][year].append(gimme_dat_info(url[0]))
     f = open("./data/course_data" + str(start_year) + ".json", "w")
     f.write(json.dumps(save))
 
-find_and_save_all_data(17) #finds all 17/18 data!
+'''
+find_and_save_all_data(17)  # finds all 17/18 data!
 find_and_save_all_data(16)
 find_and_save_all_data(15)
 find_and_save_all_data(14)
 find_and_save_all_data(13)
+'''
